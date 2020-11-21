9aebf886c1e4d817c96d522b6939eb5885d838ab
Miguel Macias <miguelfernandomacias@gmail.com>
2020-11-21 17:56:35 -0400

    books,gemfile,models,devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :controllers
+  resources :books
+  get 'home/index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
 end
